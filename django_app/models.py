17f54f5c37900b81891659f248c5831d093fda91
arturojimenezg15 <arturojimenezg15@gmail.com>
2025-10-23 18:53:31 -0400

    anadido los modelos y la inicial migracion

--- django_app/models.py
+++ django_app/models.py
@@ -5,4 +5,5 @@ from django.db import models
 class Post(models.Model):
     text = models.TextField() 
 
-    def __str__(self):
+def __str__(self):
+    return self.text[:50]  # Return the first 50 characters of the post text
